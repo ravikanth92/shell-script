70b00fb35b206f1911c02f31466ab771d99678a4
ravikanthreddy <ravikanth.ravi456@gmail.com>
2025-03-10 21:18:26 +0530

    arrays script added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -4,6 +4,6 @@ MOVIES=("pushpa" "rrr" "devara")
 
 echo "first movie: ${movie[0]}"
 echo "second movie: ${movie[1]}"
-echo "third movie: ${movie[3]}"
+echo "third movie: ${movie[2]}"
 
 echo "all movies are: ${movie[@]}"
