a45ca0ce50d8edb7ffc2a462f55f14b73fef551f
ravikanthreddy <ravikanth.ravi456@gmail.com>
2025-03-10 21:16:36 +0530

    arrays script added

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("pushpa" "rrr" "devara")
+
+echo "first movie: ${movie[0]}"
+echo "second movie: ${movie[1]}"
+echo "third movie: ${movie[3]}"
+
+echo "all movies are: ${movie[@]}"
