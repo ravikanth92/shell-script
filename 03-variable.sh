4a251b9187523c940959447a9947a32f7e83ad8d
ravikanthreddy <ravikanth.ravi456@gmail.com>
2025-03-10 12:31:13 +0530

    converstion added

--- 03-variable.sh
+++ 03-variable.sh
@@ -6,4 +6,4 @@ PERSON2=suresh
 echo "$PERSON1:: hi $PERSON2"
 echo "$PERSON2:: hi $PERSON1"
 echo "$PERSON1:: how are you doing"
-echo "$PERSON2t:: iam good. ehat about you"
+echo "$PERSON2:: iam good. ehat about you"
