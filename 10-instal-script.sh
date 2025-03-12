fe4355c061b3ffb08f3aed66d0c94c997493e24c
ravikanthreddy <ravikanth.ravi456@gmail.com>
2025-03-12 11:54:58 +0530

    instalation script added

--- 10-instal-script.sh
+++ 10-instal-script.sh
@@ -2,9 +2,9 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0] 
+if [ $USERID -ne 0 ]  
 then
-    echo"ERROR:: you must have sudo access to execute this script"
+    echo "ERROR:: you must have sudo access to execute this script"
 fi
 
 dnf install mysqll -y
